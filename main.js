38a85d93c9cf9d166da050220fe9f7edcdd650f8
The PROgrammer / <codingplusgaming@gmail.com>
2022-05-23 20:48:17 -0700

    done

---
+++ main.js
@@ -0,0 +1,16 @@
+const canvas1 = document.getElementById("canvas1");
+const ctx = canvas1.getContext("2d");
+canvas1.width = window.innerWidth;
+canvas1.height = window.innerHeight;
+
+Window.addEventListener('resize', function() {
+    canvas1.width = window.innerWidth;
+    canvas1.height = window.innerHeight;
+});
+
+ctx.fillStyle = 'red';
+ctx.strokeStyle = 'red';
+ctx.beginPath();
+ctx.arc(100, 100, 50, 0, Math.PI * 2);
+ctx.stroke();
+ctx.fill();
