65e82cac385260f5edcab0fb3708744f32175976
The PROgrammer / <codingplusgaming@gmail.com>
2022-05-24 09:04:26 -0700

    done

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-Window.addEventListener('resize', function() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-});
 
 ctx.fillStyle = 'white';
 ctx.fillRect(50, 50, 100, 100);
